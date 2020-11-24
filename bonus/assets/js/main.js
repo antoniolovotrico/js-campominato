3ee26f868e272177e8a6f3db88eacfe12600af2b
Antonio <antonio.lovotrico@icloud.com>
2020-11-24 12:41:11 +0100

    working on bonus

---
+++ bonus/assets/js/main.js
@@ -0,0 +1,61 @@
+//    BONUS
+/*all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+con difficoltà 0 => tra 1 e 100
+con difficoltà 1 =>  tra 1 e 80
+con difficoltà 2 => tra 1 e 50 */
+
+var livDiff = prompt("Scegli il livello di difficoltà al quale vuoi giocare: - 0 = Facile. - 1 = Medio. - 2 = Difficile")
+
+switch (livDiff) {
+    case 0 :
+        getRandomInt(1 , 100)
+        break;
+    case 1 :
+        getRandomInt(1 , 80)
+        break;
+    case 2 :
+        getRandomInt(1 , 50)
+        break;
+}
+
+
+
+
+//Il computer deve generare 16 numeri casuali tra 1 e 100
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; 
+}
+
+var arrNumber = [];
+while (arrNumber.length < 16) {
+    var rnNum  = function getRandomInt(min, max);
+    //console.log(rnNum);
+    if (arrNumber.includes(rnNum)== false) {
+        arrNumber.push(rnNum);
+    }
+
+}
+console.log(arrNumber);
+
+
+
+
+var arrNumberUtente = [] ;
+
+while (arrNumberUtente.length < 84 && arrNumber.includes(numUtente) == false) {   
+    var numUtente = Number(prompt("inserisci un numero da 1 a 100"));
+    if (!arrNumberUtente.includes(numUtente)) {
+        arrNumberUtente.push(numUtente);
+        console.log(arrNumberUtente);
+    } else {
+        alert("Non puoi inserire il numero nuovamente, Inserisci un altro numero");
+    }
+} 
+
+console.log("hai perso, sei riuscito ad inserire " + arrNumberUtente.length + " numeri consentiti Bravissimo!!");
+
+
+
